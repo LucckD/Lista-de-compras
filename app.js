a9c91c8093a1160b87fff4b162bfbf807fe0a261
LucckD <brnyfhegames@gmail.com>
2025-03-05 16:07:16 -0300

    Verificação de itens repetidos

--- app.js
+++ app.js
@@ -23,39 +23,50 @@ function limparCampo() {
 
 function addItem(botao) {
     let inputTexto = campo.value.trim();
-    switch (botao) {
-        case 'fru':
-            lista.fru.push(inputTexto);
-            let novoItemFru = document.createElement('p');
-            novoItemFru.textContent = inputTexto;
-            categorias.fru.appendChild(novoItemFru);
-            limparCampo();
-            break;
-        case 'lat': //linha de raciocinio:
-            lista.lat.push(inputTexto); //adiciona o que foi digitado à lista
-            let novoItemLat = document.createElement('p'); //cria um novo elemento PARAGRAFO
-            novoItemLat.textContent = inputTexto; //diz qual será o conteúdo do PARAGRAFO, que no caso vai ser o texto digitado no input
-            categorias.lat.appendChild(novoItemLat); //aonde vai ser adicionado (categoria LATICINIO) e qual será a nova adição, que no caso é o conteúdo do paragrafo
-            limparCampo();
-            break;
-        case 'frio':
-            lista.frio.push(inputTexto);
-            let novoItemFrio = document.createElement('p');
-            novoItemFrio.textContent = inputTexto;
-            categorias.frio.appendChild(novoItemFrio)
-            limparCampo();
-            break;
-        case 'outros':
-            lista.outros.push(inputTexto);
-            let novoItemOutros = document.createElement('p');
-            novoItemOutros.textContent = inputTexto;
-            categorias.outros.appendChild(novoItemOutros);
-            limparCampo();
-            break;
-        default:
-            //sempre adicionar um default para evitar possiveis erros.
-            alert('Categoria não encontrada.');
-            break;
+
+    //Pega todos os elementos da lista e transforma em um único array.
+    let todosItens = Object.values(categorias).flatMap(lista => Array.from(lista.children).map(li => li.textContent));
+
+    if (inputTexto.length === 0) {
+        alert('Por favor, insira um item.');
+    } else if (todosItens.includes(inputTexto)) {
+        alert('Este item já foi inserido.');
+        return;
+    } else {
+        switch (botao) {
+            case 'fru':
+                lista.fru.push(inputTexto);
+                let novoItemFru = document.createElement('p');
+                novoItemFru.textContent = inputTexto;
+                categorias.fru.appendChild(novoItemFru);
+                limparCampo();
+                break;
+            case 'lat': //linha de raciocinio:
+                lista.lat.push(inputTexto); //adiciona o que foi digitado à lista
+                let novoItemLat = document.createElement('p'); //cria um novo elemento PARAGRAFO
+                novoItemLat.textContent = inputTexto; //diz qual será o conteúdo do PARAGRAFO, que no caso vai ser o texto digitado no input
+                categorias.lat.appendChild(novoItemLat); //aonde vai ser adicionado (categoria LATICINIO) e qual será a nova adição, que no caso é o conteúdo do paragrafo
+                limparCampo();
+                break;
+            case 'frio':
+                lista.frio.push(inputTexto);
+                let novoItemFrio = document.createElement('p');
+                novoItemFrio.textContent = inputTexto;
+                categorias.frio.appendChild(novoItemFrio)
+                limparCampo();
+                break;
+            case 'outros':
+                lista.outros.push(inputTexto);
+                let novoItemOutros = document.createElement('p');
+                novoItemOutros.textContent = inputTexto;
+                categorias.outros.appendChild(novoItemOutros);
+                limparCampo();
+                break;
+            default:
+                //sempre adicionar um default para evitar possiveis erros.
+                alert('Categoria não encontrada.');
+                break;
+        }
     }
 }
 
